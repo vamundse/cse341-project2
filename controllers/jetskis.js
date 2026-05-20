60906b6e232f0ac6338498216121f8c8ca63ec05
vamundse <vamundse@gmail.com>
2026-05-20 16:31:46 +0200

    made jetski routes and functions for update, delete and create

--- controllers/jetskis.js
+++ controllers/jetskis.js
@@ -2,7 +2,7 @@ const { ObjectId } = require('mongodb');
 const mongodb = require('../data/database');
 const objectId = require('mongodb').ObjectId;
 
-const getAll = async (req, res) => {
+const getAllJetskis = async (req, res) => {
     const result = await mongodb.getDatabase().db().collection('jetskis').find();
     result.toArray().then((jetskis) => {
         res.setHeader('Content-Type', 'application/json');
@@ -10,13 +10,60 @@ const getAll = async (req, res) => {
     });
 };
 
-const getOne = async (req, res) => {
-    const userId = new ObjectId(req.param.id);
-    const result = await mongodb.getDatabase().db().collection('jetskis').find({_id: userId});
+const getOneJetski = async (req, res) => {
+    const jetskiId = new ObjectId(req.param.id);
+    const result = await mongodb.getDatabase().db().collection('jetskis').find({_id: jetskiId});
     result.toArray().then((jetskis) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(jetskis[0]);
     });
 };
 
-module.exports = { getAll, getOne}
+const addJetski = async (req, res) => {
+    const jetski = {
+        brand: req.body.brand,
+        model: req.body.model,
+        horsepower: req.body.horespower,
+        weight: req.body.weight,
+        storage: req.body.storage,
+        persons: req.body.persons,
+        fueltank: req.body.fueltank
+    };
+    const response = await mongodb.getDatabase().db().collection('boats').insertOne(jetski);
+    if (response.acknowledged) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || "Some error occured while updating the boat.")
+    }
+};
+
+const updateJetski = async (req, res) => {
+    const jetskiId = new ObjectId(req.params.id);
+    const jetski = {
+        brand: req.body.brand,
+        model: req.body.model,
+        horsepower: req.body.horespower,
+        weight: req.body.weight,
+        storage: req.body.storage,
+        persons: req.body.persons,
+        fueltank: req.body.fueltank
+    };
+    const response = await mongodb.getDatabase().db().collection('boats').replaceOne({_id: jetskiId}, jetski);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || "Some error occured while updating the boat.")
+    }
+};
+
+const deleteJetski = async (req, res) => {
+    const jetskiId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('boats').deleteOne({_id: jetskiId});
+    if (response.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || "Some error occured while updating the boat.")
+    }
+};
+
+module.exports = { getAllJetskis, getOneJetski, addJetski, updateJetski, deleteJetski };
--- routes/jetskis.js
+++ routes/jetskis.js
@@ -3,5 +3,8 @@ const jetskisController = require('../controllers/jetskis')
 
 routes.get('/', jetskisController.getAll);
 routes.get('/:id', jetskisController.getOne);
+routes.post('/', boatsController.addBoat);
+routes.put('/:id', boatsController.updateBoat);
+routes.delete('/:id', boatsController.deleteBoat);
 
 module.exports = routes;
