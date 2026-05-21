11c4d75c8a150a8e77d3a24f2e1c43f9f6b46102
vamundse <vamundse@gmail.com>
2026-05-21 18:44:07 +0200

    added error handling in the controller

--- controllers/boats.js
+++ controllers/boats.js
@@ -3,24 +3,48 @@ const mongodb = require('../data/database');
 const objectId = require('mongodb').ObjectId;
 
 const getAllBoats = async (req, res) => {
-    const result = await mongodb.getDatabase().db().collection('boats').find();
-    result.toArray().then((boats) => {
+    try {
+
+        const result = await mongodb.getDatabase().db().collection('boats').find();
+        const boats = await result.toArray();
+
+        if (boats.length === 0) {
+            return res.status(404).json({ message: 'No boats found' });
+        }
+
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(boats);
-    });
+
+    } catch(error) {
+        console.log('Error fetching boats', error);
+        return res.status(500).json({ message: 'Internal server error'});
+    }
 };
 
 const getOneBoat = async (req, res) => {
-    const boatId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('boats').find({_id: boatId});
-    result.toArray().then((boats) => {
+    try {
+
+        const boatId = new ObjectId(req.params.id);
+        const result = await mongodb.getDatabase().db().collection('boats').find({_id: boatId});
+        const boats = await result.toArray();
+
+        if (boats.length === 0) {
+            return res.status(404).json({ message: 'The boat was not found' });
+        }
+
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(boats[0]);
-    });
+
+    } catch (error) {
+        console.log('Error fetching the specific boat', error);
+        return res.status(500).json({ message: 'Internal server error'})
+    }
 };
 
 const addBoat = async (req, res) => {
-    const boat = {
+    try {
+
+        const boat = {
         brand: req.body.brand,
         model: req.body.model,
         year: req.body.year,
@@ -29,43 +53,65 @@ const addBoat = async (req, res) => {
         length: req.body.length,
         fuel: req.body.fuel,
         material: req.body.material
-    };
-    const response = await mongodb.getDatabase().db().collection('boats').insertOne(boat);
-    if (response.acknowledged) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || "Some error occured while updating the boat.")
+        }
+
+        const response = await mongodb.getDatabase().db().collection('boats').insertOne(boat);
+        if (result.acknowledged) {
+            res.status(201).send();
+        }
+    
+    } catch(error) {
+        console.log('There was an error while adding the boat', error);
+        return res.status(500).json("Some error occured while adding the boat.");
     }
 };
 
 const updateBoat = async (req, res) => {
-    const boatId = new ObjectId(req.params.id);
-    const boat = {
-        brand: req.body.brand,
-        model: req.body.model,
-        year: req.body.year,
-        type: req.body.type,
-        class: req.body.class,
-        length: req.body.length,
-        fuel: req.body.fuel,
-        material: req.body.material
-    };
-    const response = await mongodb.getDatabase().db().collection('boats').replaceOne({_id: boatId}, boat);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || "Some error occured while updating the boat.")
+    try {
+
+        const boatId = new ObjectId(req.params.id);
+        const boat = {
+            brand: req.body.brand,
+            model: req.body.model,
+            year: req.body.year,
+            type: req.body.type,
+            class: req.body.class,
+            length: req.body.length,
+            fuel: req.body.fuel,
+            material: req.body.material
+        };
+
+        const response = await mongodb.getDatabase().db().collection('boats').replaceOne({_id: boatId}, boat);
+        if (response.modifiedCount > 0) {
+            return res.status(204).send();
+        } else {
+            console.log('There was an error while updating the boat');
+            return res.status(404).json("The boat could not be updated");
+        }
+
+    } catch(error) {
+        console.log('There was an error while updating the boat', error);
+        return res.status(500).json({ message: 'Internal server error'});
     }
 };
 
 const deleteBoat = async (req, res) => {
-    const boatId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('boats').deleteOne({_id: boatId});
-    if (response.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || "Some error occured while updating the boat.")
+    try {
+
+        const boatId = new ObjectId(req.params.id);
+        const response = await mongodb.getDatabase().db().collection('boats').deleteOne({_id: boatId});
+
+        if (response.deletedCount > 0) {
+            return res.status(204).send();
+        } else {
+            return res.status(500).json("Some error occured while updating the boat.");
+        }
+
+    } catch(error) {
+        console.log('There was an error while deleting the boat', error);
+        return res.status(500).json({ message: 'Internal server error'});
     }
+    
 };
 
 module.exports = { getAllBoats, getOneBoat, updateBoat, addBoat, deleteBoat }
--- controllers/jetskis.js
+++ controllers/jetskis.js
@@ -3,67 +3,115 @@ const mongodb = require('../data/database');
 const objectId = require('mongodb').ObjectId;
 
 const getAllJetskis = async (req, res) => {
-    const result = await mongodb.getDatabase().db().collection('jetskis').find();
-    result.toArray().then((jetskis) => {
+    try {
+        
+        const result = await mongodb.getDatabase().db().collection('jetskis').find();
+        const jetskis = await result.toArray();
+
+        if(jetskis.length === 0) {
+            console.log('Error while fetching jetskis');
+            return res.status(404).json({ message: 'An error occured while fetching the jetskis'});
+        }
+
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(jetskis);
-    });
-};
+
+    } catch(error) {
+        console.log('Error fetching jetskis', error);
+        return res.status(500).json({ message: 'Internal server error'});
+    }
+}
 
 const getOneJetski = async (req, res) => {
-    const jetskiId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('jetskis').find({_id: jetskiId});
-    result.toArray().then((jetskis) => {
+    try {
+
+        const jetskiId = new ObjectId(req.params.id);
+        const result = await mongodb.getDatabase().db().collection('jetskis').find({_id: jetskiId});
+        const jetskis = await result.toArray();
+
+        if(jetskis.length === 0) {
+            console.log('Error while fetching one jetski');
+            return res.status(404).json({ message: 'There was an error while fetching the jetski'});
+        }
+
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(jetskis[0]);
-    });
-};
+        return res.status(200).json(jetskis[0]);
+
+    } catch(error) {
+        console.log('Error while fetching one jetski', error);
+        return res.status(500).json({ message: 'Internal server error'});
+    }
+}
 
 const addJetski = async (req, res) => {
-    const jetski = {
+    try {
+
+        const jetski = {
         brand: req.body.brand,
         model: req.body.model,
-        horsepower: req.body.horespower,
+        horsepower: req.body.horsepower,
         weight: req.body.weight,
         storage: req.body.storage,
         persons: req.body.persons,
         fueltank: req.body.fueltank
-    };
-    const response = await mongodb.getDatabase().db().collection('boats').insertOne(jetski);
-    if (response.acknowledged) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || "Some error occured while updating the boat.")
+        }
+
+        const response = await mongodb.getDatabase().db().collection('jetskis').insertOne(jetski);
+        if (response.acknowledged) {
+            return res.status(201).send();
+        }
+
+    } catch(error) {
+        console.log('Error while adding jetski', error);
+        return res.status(500).json({ message: 'There was an error while adding the jetski'});
     }
-};
+}
 
 const updateJetski = async (req, res) => {
-    const jetskiId = new ObjectId(req.params.id);
-    const jetski = {
+    try {
+
+        const jetskiId = new ObjectId(req.params.id);
+        const jetski = {
         brand: req.body.brand,
         model: req.body.model,
-        horsepower: req.body.horespower,
+        horsepower: req.body.horsepower,
         weight: req.body.weight,
         storage: req.body.storage,
         persons: req.body.persons,
         fueltank: req.body.fueltank
-    };
-    const response = await mongodb.getDatabase().db().collection('boats').replaceOne({_id: jetskiId}, jetski);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || "Some error occured while updating the boat.")
+        }
+
+        const response = await mongodb.getDatabase().db().collection('jetskis').replaceOne({_id: jetskiId}, jetski);
+        if (response.modifiedCount > 0) {
+            return res.status(204).send();
+        } else {
+            console.log('Error while updating jetski');
+            return res.status(404).json({ message: 'The jetski could not be updated'});
+        }
+
+    } catch(error) {
+        console.log('Error while updating jetski', error);
+        return res.status(500).json({ message: 'Internal server error'});
     }
-};
+}
 
 const deleteJetski = async (req, res) => {
-    const jetskiId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('boats').deleteOne({_id: jetskiId});
-    if (response.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || "Some error occured while updating the boat.")
+    try {
+    
+        const jetskiId = new ObjectId(req.params.id);
+        const response = await mongodb.getDatabase().db().collection('jetskis').deleteOne({_id: jetskiId});
+
+        if (response.deletedCount > 0) {
+            return res.status(204).send();
+        } else {
+            console.log('Error while deleting jetski');
+            return res.status(404).json({ message: 'The jetski could not be deleted' });
+        }
+
+    } catch(error) {
+        console.log('Error while deleting jetski', error);
+        return res.status(500).json({ message: 'Internal server error'});
     }
-};
+}
 
 module.exports = { getAllJetskis, getOneJetski, addJetski, updateJetski, deleteJetski };
