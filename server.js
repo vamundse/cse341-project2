37bee58b1e00197b044235048bba0637556b5540
vamundse <vamundse@gmail.com>
2026-05-21 18:46:47 +0200

    added bodyparser,api-docs route and error handling

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT;
 
+app.use('/api-docs', require('./routes/swagger'));
 app.use('/', require('./routes'));
 app.use(bodyParser.json());
+
+process.on('uncaughtException', (err, origin) => {
+    console.error(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin} `);
+});
 
 mongodb.initDb((err) => {
     if(err) {
