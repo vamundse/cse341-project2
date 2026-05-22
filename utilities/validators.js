1aa95e7ac293bffe2422e01115e3ee82deeb9164
vamundse <vamundse@gmail.com>
2026-05-22 10:54:23 +0200

    Added different validations rules for post and put

--- utilities/validators.js
+++ utilities/validators.js
@@ -1,6 +1,6 @@
 const { body, validationResult } = require('express-validator');
 
-const boatValidationRules = () => {
+const boatAddValidationRules = () => {
     return [
         body('brand')
         .notEmpty()
@@ -47,7 +47,48 @@ const boatValidationRules = () => {
     ]
 }
 
-const jetskiValidationRules = () => {
+const boatEditValidationRules = () => {
+    return [
+        body('brand')
+        .isString()
+        .withMessage('Brand must be a string')
+        .isLength({ min: 3 })
+        .withMessage('Brand must be minimum three characters'),
+        body('model')
+        .isString()
+        .withMessage('Model must be a string')
+        .isLength({ min: 3 })
+        .withMessage('Model must be minimum three characters'),
+        body('year')
+        .isInt({ min: 1900, max: 2100})
+        .withMessage('The year must be a 4-digit number'),
+        body('type')
+        .isString()
+        .withMessage('Type must be a string')
+        .isLength({ min: 3 })
+        .withMessage('Type must be minimum three characters'),
+        body('class')
+        .isString()
+        .withMessage('Class must be a string')
+        .isLength({ min: 3 })
+        .withMessage('Class must be minimum three characters'),
+        body('length')
+        .isString()
+        .withMessage('Length must be a string'),
+        body('fuel')
+        .isString()
+        .withMessage('Fuel must be a string')
+        .isLength({ min: 3 })
+        .withMessage('Fuel must be minimum three characters'),
+        body('material')
+        .isString()
+        .withMessage('Material must be a string')
+        .isLength({ min: 3 })
+        .withMessage('Material must be minimum three characters'),
+    ]
+}
+
+const jetskiAddValidationRules = () => {
     return [
         body('brand')
         .notEmpty()
@@ -59,6 +100,46 @@ const jetskiValidationRules = () => {
         body('model')
         .notEmpty()
         .withMessage('Model is required')
+        .isString()
+        .withMessage('Model must be a string')
+        .isLength({ min: 3 })
+        .withMessage('Model must be minimum three characters'),
+        body('horsepower')
+        .isString()
+        .withMessage('Horsepower must be a string')
+        .isLength({ min: 3 })
+        .withMessage('Horsepower must be minimum three characters'),
+        body('weight')
+        .isString()
+        .withMessage('Weight must be a string')
+        .isLength({ min: 3 })
+        .withMessage('Weight must be minimum three characters'),
+        body('storage')
+        .isString()
+        .withMessage('Storage must be a string')
+        .isLength({ min: 3 })
+        .withMessage('Storage must be minimum three characters'),
+        body('persons')
+        .isString()
+        .withMessage('Persons must be a string')
+        .isLength({ min: 3 })
+        .withMessage('Persons must be minimum three characters'),
+        body('fueltank')
+        .isString()
+        .withMessage('Fueltank must be a string')
+        .isLength({ min: 3 })
+        .withMessage('Fueltank must be minimum three characters'),
+    ]
+}
+
+const jetskiEditValidationRules = () => {
+    return [
+        body('brand')
+        .isString()
+        .withMessage('Brand must be a string')
+        .isLength({ min: 3 })
+        .withMessage('Brand must be minimum three characters'),
+        body('model')
         .isString()
         .withMessage('Model must be a string')
         .isLength({ min: 3 })
