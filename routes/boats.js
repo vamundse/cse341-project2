bac513d283f756ab7c1718c14bc5ddb987cee4e0
vamundse <vamundse@gmail.com>
2026-05-22 10:45:25 +0200

    validatorrules were called incorrectly, fixed it

--- routes/boats.js
+++ routes/boats.js
@@ -4,8 +4,8 @@ const validator = require('../utilities/validators')
 
 router.get('/', boatsController.getAllBoats);
 router.get('/:id', boatsController.getOneBoat);
-router.post('/', validator.boatValidationRules, validator.validate, boatsController.addBoat);
-router.put('/:id', validator.boatValidationRules, validator.validate, boatsController.updateBoat);
+router.post('/', validator.boatValidationRules(), validator.validate, boatsController.addBoat);
+router.put('/:id', validator.boatValidationRules(), validator.validate, boatsController.updateBoat);
 router.delete('/:id', boatsController.deleteBoat);
 
 module.exports = router;
