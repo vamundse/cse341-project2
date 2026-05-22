9e060e32e776a9bb688d460b5139d31179eea9fc
vamundse <vamundse@gmail.com>
2026-05-22 10:54:42 +0200

    added the new validations rules for post and put

--- routes/jetskis.js
+++ routes/jetskis.js
@@ -4,8 +4,8 @@ const validator = require('../utilities/validators')
 
 router.get('/', jetskisController.getAllJetskis);
 router.get('/:id', jetskisController.getOneJetski);
-router.post('/', validator.jetskiValidationRules(), validator.validate, jetskisController.addJetski);
-router.put('/:id', validator.jetskiValidationRules(), validator.validate, jetskisController.updateJetski);
+router.post('/', validator.jetskiAddValidationRules(), validator.validate, jetskisController.addJetski);
+router.put('/:id', validator.jetskiEditValidationRules(), validator.validate, jetskisController.updateJetski);
 router.delete('/:id', jetskisController.deleteJetski);
 
 module.exports = router;
