f2fd38661d45f85d290db3dc62899c8852bb8fdb
vamundse <vamundse@gmail.com>
2026-05-22 09:51:13 +0200

    Added validation to the put and post routes.

--- routes/boats.js
+++ routes/boats.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const boatsController = require('../controllers/boats');
+const validator = require('../utilities/validators')
 
 router.get('/', boatsController.getAllBoats);
 router.get('/:id', boatsController.getOneBoat);
-router.post('/', boatsController.addBoat);
-router.put('/:id', boatsController.updateBoat);
+router.post('/', validator.boatValidationRules, validator.validate, boatsController.addBoat);
+router.put('/:id', validator.boatValidationRules, validator.validate, boatsController.updateBoat);
 router.delete('/:id', boatsController.deleteBoat);
 
 module.exports = router;
--- routes/jetskis.js
+++ routes/jetskis.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const jetskisController = require('../controllers/jetskis')
+const validator = require('../utilities/validators')
 
 router.get('/', jetskisController.getAllJetskis);
 router.get('/:id', jetskisController.getOneJetski);
-router.post('/', jetskisController.addJetski);
-router.put('/:id', jetskisController.updateJetski);
+router.post('/', validator.jetskiValidationRules, validator.validate, jetskisController.addJetski);
+router.put('/:id', validator.jetskiValidationRules, validator.validate, jetskisController.updateJetski);
 router.delete('/:id', jetskisController.deleteJetski);
 
 module.exports = router;
