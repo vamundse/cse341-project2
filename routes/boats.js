eb3f7425054993938878264dc1d937ff27cc36dd
vamundse <vamundse@gmail.com>
2026-05-27 08:34:51 +0200

    added is authenticated to post, put and delete routes

--- routes/boats.js
+++ routes/boats.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const boatsController = require('../controllers/boats');
-const validator = require('../utilities/validators')
+const validator = require('../utilities/validators');
+const { isAuthenticated } = require('../utilities/authenticate');
 
 router.get('/', boatsController.getAllBoats);
 router.get('/:id', boatsController.getOneBoat);
-router.post('/', validator.boatAddValidationRules(), validator.validate, boatsController.addBoat);
-router.put('/:id', validator.boatEditValidationRules(), validator.validate, boatsController.updateBoat);
-router.delete('/:id', boatsController.deleteBoat);
+router.post('/', isAuthenticated, validator.boatAddValidationRules(), validator.validate, boatsController.addBoat);
+router.put('/:id', isAuthenticated, validator.boatEditValidationRules(), validator.validate, boatsController.updateBoat);
+router.delete('/:id', isAuthenticated, boatsController.deleteBoat);
 
 module.exports = router;
--- routes/jetskis.js
+++ routes/jetskis.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const jetskisController = require('../controllers/jetskis')
 const validator = require('../utilities/validators')
+const { isAuthenticated } = require('../utilities/authenticate');
 
 router.get('/', jetskisController.getAllJetskis);
 router.get('/:id', jetskisController.getOneJetski);
-router.post('/', validator.jetskiAddValidationRules(), validator.validate, jetskisController.addJetski);
-router.put('/:id', validator.jetskiEditValidationRules(), validator.validate, jetskisController.updateJetski);
-router.delete('/:id', jetskisController.deleteJetski);
+router.post('/', isAuthenticated, validator.jetskiAddValidationRules(), validator.validate, jetskisController.addJetski);
+router.put('/:id', isAuthenticated, validator.jetskiEditValidationRules(), validator.validate, jetskisController.updateJetski);
+router.delete('/:id', isAuthenticated, jetskisController.deleteJetski);
 
-module.exports = router;
+module.exports = router; 
