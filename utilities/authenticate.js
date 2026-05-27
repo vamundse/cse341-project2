77886371f15c1e747f48faac72b0cd1333a8bf88
vamundse <vamundse@gmail.com>
2026-05-27 09:28:27 +0200

    changed to req.user

--- utilities/authenticate.js
+++ utilities/authenticate.js
@@ -1,5 +1,5 @@
 const isAuthenticated = (req, res, next) => {
-    if (req.session.user === undefined){
+    if (req.user === undefined){
         return res.status(401).json('You do not have access');
     }
     next();
