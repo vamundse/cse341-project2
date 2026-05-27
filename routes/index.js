3516e05e49bede6fcf871e9ad60c2e38a443c083
vamundse <vamundse@gmail.com>
2026-05-27 08:34:34 +0200

    made a login and logout route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
+const passport = require('passport');
 
 router.get('/', (req, res) => {res.send('Hello World')});
 router.use('/boats', require('./boats'));
-router.use('/jetskis', require('./jetskis'))
+router.use('/jetskis', require('./jetskis'));
+
+router.get('/login', passport.authenticate('google', (req, res) => {}));
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/')
+    });
+});
 
 module.exports = router;
